f8c83c9a8d3067433a14e1d7cc128bbd73273163
meitar <18243325+profawk@users.noreply.github.com>
2020-10-03 20:26:58 +0300

    added with func for pr 239 hotfix

--- main.go
+++ main.go
@@ -21,6 +21,14 @@ var (
 
 	delKeyboard = &tb.ReplyMarkup{}
 	delInline   = delKeyboard.Data("🗑️ Cancel", "delete").Inline()
+
+	addKeyboard  = &tb.ReplyMarkup{}
+	addApiStatus = addKeyboard.Data("Get status", "status").Inline()
+	addApiOn     = addKeyboard.Data("Turn on", "on").Inline()
+	addApiOff    = addKeyboard.Data("Turn off", "off").Inline()
+	addRecurring = addKeyboard.Data("Recurring", "recurring").Inline()
+	addDone      = addKeyboard.Data("Done", "done").Inline()
+	addCancel    = addKeyboard.Data("Cancel", "cancel").Inline()
 )
 
 func init() {
@@ -29,6 +37,13 @@ func init() {
 		menu.Row(btnOn, btnOff),
 		menu.Row(btnSchedule),
 	)
+}
+
+// until https://github.com/tucnak/telebot/pull/324 gets approved
+func with(b *tb.InlineButton, data string) *tb.InlineButton {
+	nb := b.With(data)
+	nb.URL = b.URL
+	return nb
 }
 
 func validateChatIds(upd *tb.Update) bool {
@@ -61,6 +76,37 @@ func apiMiddleware(b *tb.Bot, apiCall func() api.State) func(m *tb.Message) {
 	return func(m *tb.Message) {
 		sendApiMessage(b, m.Sender, "The relay is %s", apiCall)
 	}
+}
+
+func getAddKeyboard(repr taskRepr) *tb.ReplyMarkup {
+	marsh, _ := repr.MarshalText()
+	data := string(marsh)
+	keyboard := [][]tb.InlineButton{
+		{
+			*with(addApiStatus, data),
+			*with(addApiOn, data),
+			*with(addApiOff, data),
+		},
+		{
+			*with(addRecurring, data),
+		},
+		{
+			*with(addDone, data),
+			*with(addCancel, data),
+		},
+	}
+	for i, b := range keyboard[0] {
+		//color := "\U0001F534  "
+		color := "\u26aa  "
+		if b.Unique == repr.what {
+			color = "\U0001F7E2  "
+		}
+		keyboard[0][i].Text = color + keyboard[0][i].Text
+	}
+	if repr.recurring {
+		keyboard[1][0].Text = "\U0001F7E2  " + keyboard[1][0].Text
+	}
+	return &tb.ReplyMarkup{InlineKeyboard: keyboard}
 }
 
 func parseTime(s string) (time.Time, error) {
@@ -154,6 +200,19 @@ func main() {
 		b.Send(m.Sender, "Task has been added\n"+task.String(), menu)
 	})
 
+	b.Handle("/schedule2", func(m *tb.Message) {
+		t, err := parseTime(m.Payload)
+		if err != nil {
+			b.Send(m.Sender, "Could not parse time. is it in [dd/mm] hh:mm")
+			log.Print(err)
+			return
+		}
+		repr := taskRepr{
+			when: t,
+		}
+		b.Send(m.Sender, "Great! now choose what to do", getAddKeyboard(repr))
+	})
+
 	b.Handle(&btnStatus, apiMiddleware(b, a.Status))
 	b.Handle(&btnOn, apiMiddleware(b, a.TurnOn))
 	b.Handle(&btnOff, apiMiddleware(b, a.TurnOff))
@@ -162,8 +221,7 @@ func main() {
 		s.L.Lock()
 		defer s.L.Unlock()
 		for id, t := range s.Tasks {
-			btn := delInline.With(id)
-			btn.URL = "" // until https://github.com/tucnak/telebot/pull/324 gets approved
+			btn := with(delInline, id)
 			delKeyboard.InlineKeyboard = [][]tb.InlineButton{{*btn}}
 			b.Send(m.Sender, t.String(), delKeyboard)
 		}
